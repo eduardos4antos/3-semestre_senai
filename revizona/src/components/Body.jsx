67554c14390e648cb4fd995c005cdd40ac292059
eduardos4antos <eduardo_hf_santos@estudante.sesisenai.org.br>
2025-02-06 16:35:15 -0300

    primeira aula porra

---
+++ revizona/src/components/Body.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Body.css';
+
+function Body() {
+  return (
+    <div className="container-body">
+      <p>Top Motivos do Darlan Gostar de Cavalos</p>
+      <ul>
+        <li>Ele gosta de cavalos pois são legais</li>
+        <li>Ele gosta de cavalos porque são bonitos</li>
+        <li>Ele gosta de cavalos porque são grandes</li>
+        <li>Ele gosta de cavalos porque são robustos</li>
+        <li>Ele gosta de cavalos porque são CAVALOS</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Body;
---
+++ revizona/src/components/Header.jsx
@@ -0,0 +1,10 @@
+import './Header.css'
+function Header() {
+  return (
+    <div className="container-header">
+        <p>darlan amante de cavalos</p>
+    </div>
+  )
+}
+
+export default Header
